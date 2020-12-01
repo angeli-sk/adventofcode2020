6814c6b71d5e928b7920df5db93c06187554977d
angeli-sk <solange@kalea.nl>
2020-12-01 15:47:57 +0100

    day01 passed

---
+++ Day1/part1.cpp
@@ -0,0 +1,69 @@
+#include <fstream>
+#include <string.h>
+#include <iostream>
+#include "colors.h/colors.h"
+
+int line_count(std::string file_name)
+{
+    std::ifstream file(file_name);
+    std::string line;
+    int i = 0;
+
+    while (getline (file, line))
+        i++;
+    file.close(); 
+    return (i);
+}
+
+int twoOtwoO_finder(int *array, int length)
+{
+    int x = 0;
+    int y = 1;
+
+    while (x < length)
+    {
+        while (y < length)
+        {
+            if ((array[x] + array[y]) == 2020)
+                return (array[x] * array[y]);
+            y++;
+        }
+       
+        x++; 
+        y = x + 1;
+    }
+    return(-1);
+}
+
+int main(void)
+{
+    std::string line;
+    int *array;
+    int num = 0;
+    int length = 0;
+  
+    std::ifstream file("input.txt");
+    if(file.fail())
+        return (-1);
+    length = line_count("input.txt");
+    array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        getline (file, line);
+        num = std::stoi(line);
+        array[i] = num;
+    }
+    num = twoOtwoO_finder(array, length);
+    if (num == -1)
+    {
+        std::cout << COLOR_RED << "IT FAIL LOSER!" << std::endl;
+        return(-1);
+    }
+    std::cout << COLOR_YELLOW << "part1: 2020 " 
+        << COLOR_RED << "vision "
+        << COLOR_GREEN <<  "be like: " 
+        << COLOR_GREEN << num << std::endl;
+    delete[] array;
+    file.close(); 
+    return(0);
+}
